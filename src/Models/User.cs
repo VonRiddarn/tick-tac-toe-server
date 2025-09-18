87113336a2655df6fd75ccfef05a21a929c0d7f5
VonRiddarn <vonriddarn@gmail.com>
2025-09-18 18:59:29 +0200

    feat: add default value to expires_at to make sure we don't crash

--- src/Models/User.cs
+++ src/Models/User.cs
@@ -5,6 +5,7 @@ namespace TimmyOhman.TicTacToeServer.Models
 		public string Username { get; set; } = "UNKNOWN_USER";
 		public string PasswordHash { get; set; } = "PASSWORD HASH";
 		public string? AuthToken { get; set; }
+		public DateTime ExpiresAt { get; set; } = DateTime.Now;
 		public int SelectedImage {get; set; } = 0;
 		public string Token { get; set; } = string.Empty;
 

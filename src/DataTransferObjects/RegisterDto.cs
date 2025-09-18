4d3925137529f963df0a1f77347fd625a26de628
VonRiddarn <vonriddarn@gmail.com>
2025-09-18 21:40:00 +0200

    feat: implement user registration data transfer object

---
+++ src/DataTransferObjects/RegisterDto.cs
@@ -0,0 +1,10 @@
+namespace TimmyOhman.TicTacToeServer.DataTransferObjects
+{
+	public class RegisterDto
+	{
+		public string Username { get; set; } = string.Empty;
+		public string Password { get; set; } = string.Empty;
+
+		public RegisterDto() {}
+	}
+}
